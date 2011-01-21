43b1d85bb2ce532a092f3253dbb8228b2d8af924
Martin Kou <martinkou@hydrogen.(none)>
2011-01-21 00:20:40 -0800

    Bumped version to 0.3.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 from setuptools import setup, find_packages
 
 setup(name = "bson",
-		version="0.3rc",
+		version="0.3",
 		packages=["bson"],
 		install_requires = ["pytz>=2010b"],
 		author = "Kou Man Tong",

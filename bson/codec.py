daf9439703630553ffc871181587458ca99fcb91
Parkayun <iamparkayun@gmail.com>
2015-07-27 20:33:26 +0900

    Fixed encode_array for Python3

--- bson/codec.py
+++ bson/codec.py
@@ -246,6 +246,9 @@ def encode_array(array, traversal_stack, traversal_parent = None, generator_func
         traversal_stack.pop()
     e_list = buf.getvalue()
     e_list_length = len(e_list)
+    if PY3:
+        e_list = e_list.encode("utf-8")
+        return struct.pack("<i%dsb" % (e_list_length,), e_list_length + 4 + 1, e_list, 0)
     return struct.pack("<i%dsb" % (e_list_length,), e_list_length + 4 + 1, e_list, 0)
 
 

1733ed5d075cb7c6bd8bd9d7b45c95e9728fc2f5
Parkayun <iamparkayun@gmail.com>
2015-07-29 20:56:19 +0900

    Updated copyright

--- bson/codec.py
+++ bson/codec.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python -OOOO
 # vim: set fileencoding=utf8 shiftwidth=4 tabstop=4 textwidth=80 foldmethod=marker :
 # Copyright (c) 2010, Kou Man Tong. All rights reserved.
+# Copyright (c) 2015, Ayun Park. All rights reserved.
 # For licensing, see LICENSE file included in the package.
 """
 Base codec functions for bson.

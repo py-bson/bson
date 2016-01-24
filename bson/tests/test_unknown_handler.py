eea8f58d72ef5b1729ae98fecdd8ab7699de184c
Parkayun <iamparkayun@gmail.com>
2016-01-24 18:25:27 +0900

    Keep PEP8

--- bson/tests/test_unknown_handler.py
+++ bson/tests/test_unknown_handler.py
@@ -5,10 +5,11 @@ from decimal import Decimal
 from unittest import TestCase
 
 class TestUnknownHandler(TestCase):
-	def test_unknown_handler(self):
-		d = Decimal("123.45")
-		obj = {"decimal": d}
+    def test_unknown_handler(self):
+        d = Decimal("123.45")
+        obj = {"decimal": d}
 
-		serialized = dumps(obj, on_unknown=float)
-		unserialized = loads(serialized)
-		self.assertEqual(float(d), unserialized["decimal"])
+        serialized = dumps(obj, on_unknown=float)
+	unserialized = loads(serialized)
+	self.assertEqual(float(d), unserialized["decimal"])
+

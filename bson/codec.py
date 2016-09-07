9ebc50c010e561c74ce5ad55cd1119762bb00cb9
Ryan Pessa <ryan.pessa@ijettechnologies.com>
2016-09-06 17:51:03 -0700

    cast parsed boolean as bool

--- bson/codec.py
+++ bson/codec.py
@@ -279,7 +279,7 @@ def decode_document(data, base, as_array=False):
             value = b2a_hex(data[base:base + 12])
             base += 12
         elif element_type == 0x08: #  boolean
-            value = char_struct.unpack(data[base:base + 1])[0]
+            value = bool(char_struct.unpack(data[base:base + 1])[0])
             base += 1
         elif element_type == 0x09: #  UTCdatetime
             value = datetime.fromtimestamp(
